0e44503c4e5fa700f31b8a8fa46db790fc2a7721
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2025-01-18 14:55:38 -0500

    create variables and functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
 "use strict";
+const inputEl = document.getElementById('guess-input')
+const result = document.getElementById('guess-message')
+const yourGuess = document.getElementById('current-guess')
+const computerGeuss = document.getElementById('computer-guess')
+const guessHistory = document.getElementById('guess-history')
+const submitBtn = document.getElementById('submit-btn')
+const restartBtn = document.getElementById('restart-btn')
+
+let randomNumber;
+function guessNumber(){
+    yourGuess.innerText = inputEl.value
+    inputEl.value = ""
+}
+
+function computerGuessNumber(){
+    randomNumber = Math.floor(Math.random() * 10)
+    computerGeuss.innerText = randomNumber
+}
+
+function gameResult(){
+   if(inputEl.value === randomNumber){
+    result.innerText = "Won!"
+   }else{
+    result.innerText = "lose!"
+   }
+}
+
+function render (){
+    guessNumber()
+    computerGuessNumber()
+    gameResult()
+}
+
+function restartGame(){
+
+}
+
+submitBtn.addEventListener('click', function(){
+    render()
+})
+
